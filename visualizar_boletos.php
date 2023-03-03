87d69dd8634d29c8d0398b1c78607856f228e9bd
MiriiFernandez~ <you@example.com>
2023-03-03 11:59:27 +0100

    se ha hecho la suma total de la cantidad falta arreglar fallo de las transacciones

--- visualizar_boletos.php
+++ visualizar_boletos.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comprobar_boletos_ok</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section class="container">
+        <section class="logo">
+            <img src="https://www.loteriasyapuestas.es/f/loterias/estaticos/imagenes/topaz/cabecera_EuromillonesAJ_topaz.png" alt="">
+        </section>
+        <br><br>
+
+        <br><br>
+
+</body>
+
+
+
+
 <?php
 
 // Incluimos el archivo de conexion mediante PDO
@@ -30,7 +55,15 @@ try {
 
     try {
         // Ejecutar la consulta para actualizar la columna
+        $stmt = $pdo->prepare("UPDATE premios SET cantidad = 600000 WHERE boleto = 48791");
+        $stmt = $pdo->prepare("UPDATE premios SET cantidad = 10000 WHERE boleto = 48790 AND 48792");
         $stmt = $pdo->prepare("UPDATE premios SET cantidad = 300 WHERE boleto BETWEEN 48700 AND 48799");
+
+        $stmt = $pdo->prepare("UPDATE premios SET cantidad = 300 WHERE SUBSTR(boleto,-3) = 791");
+        $stmt = $pdo->prepare("UPDATE premios SET cantidad = 120 WHERE SUBSTR(boleto,-2) = 91");
+        $stmt = $pdo->prepare("UPDATE premios SET cantidad = 60 WHERE SUBSTR(boleto,-1) = 1 LIMIT 1");
+
+
         $stmt->execute();
 
         // Comprobar si hubo algún error
@@ -48,7 +81,15 @@ try {
             $datos = $consulta_origen->fetchAll(PDO::FETCH_ASSOC);
 
             foreach ($datos as $row) {
-                echo "- <b>" . $row["boleto"] . " " . " " . $row["fecha_sorteo"] . " " . $row["cantidad"] . "</b><br>";
+                echo '<p class="fila">' . $row["boleto"] . " " . " " . $row["fecha_sorteo"] . " " . $row["cantidad"] . '</p>';
+            }
+
+            echo "<br>";
+
+            //SUMA DE LA COLUMNA CANTIDAD
+            foreach ($pdo->query('SELECT SUM(cantidad) FROM premios') as $row) {
+                echo '<p class="fila">' . "Suma total de los premios: " . '</p>';
+                echo '<p class="fila">' . $row['SUM(cantidad)'] . '</p>';
             }
         }
     } catch (Exception $e) {
@@ -59,3 +100,6 @@ try {
 } catch (PDOException $e) {
     echo "Error al copiar los datos: " . $e->getMessage();
 }
+
+echo "<br>";
+echo " <a href='index.php'><button class='button-29' role='button'>Volver</button> </a>";
